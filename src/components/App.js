5084d4adb13c07a70d45aad013dff79d4b3d71be
nottak0409 <nottak0409@gmail.com>
2021-06-01 08:43:55 +0900

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const App = props => {
-
-    const [state, setState] = useState(props);
-    const reset = () => { setState(props); }
-    const { name, price } =state
-
-    useEffect(() => {
-        console.log("This is like ComponentDidMount or Componendidupdate");
-    });
-
-    useEffect(() => {
-        console.log("This is like ComponentDidMount");
-    }, []);
-
-    useEffect(() => {
-        console.log("This callback is for name only");
-    }, [name]);
-
     return (
-        <React.Fragment>
-            <p>現在の{name}は{price}円です</p>
-            <button onClick={() => setState({...state, price: price + 1})}>+1</button>
-            <button onClick={() => setState({...state, price: price + -1})}>-1</button>
-            <button onClick={reset}>reset</button>
-            <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
-        </React.Fragment>
+        <div className="container-fluid">
+        </div>
     );
-}
-
-App.defaultProps = {
-    name: '',
-    price: 1000
 }
 
 export default App;
