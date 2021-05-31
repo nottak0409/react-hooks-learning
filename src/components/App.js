e8450a8a4c93303c59f89c4a76b3c632b11062bb
nottak0409 <nottak0409@gmail.com>
2021-06-01 08:53:45 +0900

    style

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = props => {
     return (
         <div className="container-fluid">
+            <h4>イベント作成フォーム</h4>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="formEventTitle">タイトル</label>
+                    <input className="form-control" id="formEventTitle" />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="formEventBody">ボディ</label>
+                    <textarea className="form-control" id="formEventBody"></textarea>
+                </div>
+
+                <button className="btn btn-primary">イベントを作成する</button>
+                <button className="btn btn-danger">全てのイベントを削除する</button>
+            </form>
+
+            <h4>イベント一覧</h4>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>削除</th>
+                        <th>ボディ</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
         </div>
     );
 }
