3f847ac1cb10e96b9a6d4dc72d6c9d424c5cd1fe
nottak0409 <nottak0409@gmail.com>
2021-06-01 07:52:22 +0900

    all

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React, { useState } from 'react';
 
 const App = props => {
 
-    const [name, setName] = useState(props.name);
-    const [price, setPrice] = useState(props.price);
-
-    const reset = () => {
-        setPrice(props.price);
-        setName(props.name);
-    }
+    const [state, setState] = useState(props);
+    const reset = () => { setState(props); }
+    const { name, price } =state
 
     return (
         <React.Fragment>
             <p>現在の{name}は{price}円です</p>
-            <button onClick={() => setPrice(price + 1)}>+1</button>
-            <button onClick={() => setPrice(price + -1)}>-1</button>
+            <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+            <button onClick={() => setState({...state, price: price + -1})}>-1</button>
             <button onClick={reset}>reset</button>
-            <input value={name} onChange={e => setName(e.target.value)}/>
+            <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
         </React.Fragment>
     );
 }
