be92cd10a5cd3f5f71c598496902c9f2fd00a3f8
nottak0409 <nottak0409@gmail.com>
2021-06-05 09:02:02 +0900

    delete logs add

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,12 @@
 import React, { useContext } from 'react';
 
-import { DELETE_EVENT } from '../actions'
+import {
+    DELETE_EVENT,
+    ADD_OPERATION_LOG
+ } from '../actions'
 import AppContext from '../contexts/AppContext'
+
+import { timeCurrentIso8601 } from '../utils';
 
 const Event = ({ event }) => {
     const { dispatch } = useContext(AppContext)
@@ -9,7 +14,15 @@ const Event = ({ event }) => {
     const handleClickDeleteButton = () => {
         const result = window.confirm(`イベント(id=${id})を本当に削除しても良いですか？`);
         if(result) {
-            dispatch({ type: DELETE_EVENT, id})
+            dispatch({
+                type: DELETE_EVENT, id
+            })
+
+            dispatch({
+                type: ADD_OPERATION_LOG,
+                description: `イベント(id=${id})を削除しました。`,
+                operatedAt: timeCurrentIso8601
+            })
         }
     }
     return (
