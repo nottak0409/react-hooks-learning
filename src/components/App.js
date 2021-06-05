9c23ea3cde01566ca5ecd38cc65880e4543515a5
nottak0409 <nottak0409@gmail.com>
2021-06-05 09:45:09 +0900

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from './EventForm';
@@ -9,12 +9,20 @@ import AppContext from '../contexts/AppContext';
 
 import reducer from '../reducers';
 
-const App = props => {
-    const initialState = {
+const APP_KEY = 'appwithRedux'
+
+const App = () => {
+    const appState = localStorage.getItem(APP_KEY)
+    const initialState = appState ? JSON.parse(appState) : {
         events: [],
         operationLogs: []
     }
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        const string = JSON.stringify(state);
+        localStorage.setItem(APP_KEY, string)
+    }, [state])
 
     return (
         <AppContext.Provider value={{ state, dispatch }}>
